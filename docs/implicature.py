24409c79a0cf669ff5fa1506b8b598254f0f3659
marvosyntactical <koss@cl.uni-heidelberg.de>
2019-02-12 02:17:42 +0100

    examine factor

--- docs/implicature.py
+++ docs/implicature.py
@@ -11,7 +11,7 @@ import pyro.poutine as poutine
 
 from search_inference import factor, HashingMarginal, Search, memoize
 
-
+import sys
 
 #== infer in webppl
 def Marginal(func):
@@ -23,7 +23,7 @@ def Marginal(func):
 n_states = 4
 
 def state_prior():
-    #noteice argumentless, try with mock arguments?
+    #notice argumentless, try with mock arguments?
     n =pyro.sample("whyDoINeedToNameThisState", dist.Categorical(probs=torch.ones(n_states+1)/(n_states+1)))
     return n
 
@@ -42,10 +42,19 @@ def meaning(utterance, state):
     return meanings[utterance](state)
 
 for _ in range(1000):
-    utt = utterance_prior()
-    print("Utterance: "+ utt)
+    utt = utterance_prior() 
     state = state_prior()
-    print("State: ", state, "out of n=4 objects")
-    print(meaning(utterance_prior(), state_prior()))
+    #if len(sys.argv) == 3:
+    #    utt = sys.argv[-2]
+    #    myint = int(sys.argv[-1])
+    #    state = torch.tensor([myint])
+    print("State: ", int(state.item()), "out of n=4 objects")
+    print("Utterance: "+ utt)
+    print("Truth Value: ",int(meaning(utt, state).item()))
     print("\n\n")
-    
+
+
+@Marginal
+def l0(utterance):
+    state = state_prior()
+    factor("literal_meaning", 0. if meaning(utterance, state
